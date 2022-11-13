0406d3d7b17deb045f4724c35008e544c2337d58
SpyroDA <darrian.the.best@gmail.com>
2022-11-13 12:04:56 -0600

    First commit

---
+++ src/main/java/org/scoreboard/App.java
@@ -0,0 +1,29 @@
+package org.scoreboard;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Scoreboard.fxml"));
+        primaryStage.setTitle("Team Editor");
+        primaryStage.setScene(new Scene(root, 400, 230));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/org/scoreboard/Controller.java
@@ -0,0 +1,53 @@
+package org.scoreboard;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+public class Controller {
+    private final ObservableList<Team> teams = FXCollections.observableArrayList();
+
+    @FXML
+    private ListView<Team> myListView;
+
+    public void initialize() {
+        teams.clear();
+        myListView.setItems(teams);
+        myListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        myListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            // do your stuff
+            public void handle(MouseEvent event) {
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    fxmlLoader.setLocation(getClass().getResource("org/scoreboard/Editor.fxml"));
+                    Scene scene = new Scene(fxmlLoader.load(), 380, 200);
+                    Stage stage = new Stage();
+                    stage.setTitle("New Window");
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException e) {
+                    System.err.println("ERROR!!!");
+                }
+            }
+        });
+        setScoreboard();
+    }
+
+    public void setScoreboard() {
+        teams.add(new Team("Highly Irresistible Lions", 45));
+        teams.add(new Team("Immovable Tigers", 75));
+        teams.add(new Team("Super Duper Bears", 100));
+        teams.add(new Team("Incomparable Otters", 30));
+        teams.add(new Team("Resplendent Ocelots", 8));
+    }
+}
